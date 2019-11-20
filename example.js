8af460861949fbd99a0d12477c713fb79be04895
bhavayanand <bhavay.anand@intelliticks.com>
2019-11-21 00:34:03 +0530

    ping finished

--- example.js
+++ example.js
@@ -2,18 +2,7 @@ let Ping = require('./ping');
 
 let c = new Ping('15.206.102.11');
 
-c.sendPingPacket();
+// c.sendPing();
 
+// c.sendLargePingPacket();
 
-// let raw = require('raw-socket');
-// var buffer = new Buffer ([
-//     0x08, 0x00, 0x5c, 0x60, 0x00, 0x01, 0x0a, 0x09,
-//     0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68]);
-//
-// console.log(buffer);
-//
-// var socket = raw.createSocket({
-//     protocol: raw.Protocol.ICMP
-// });
-//
-// socket.send (buffer, 0, buffer.length, '15.206.102.11', beforeSend, afterSend);
--- ping.js
+++ ping.js
@@ -1,15 +1,15 @@
 let raw = require('raw-socket');
 
 class Ping {
-    constructor(destinationIp) {
-        this.socket = raw.createSocket({
+    constructor(destinationIp){
+        this.socket =    raw.createSocket({
             protocol: raw.Protocol.ICMP
         });
         this.destinationIp = destinationIp;
         this.packet = null;
     }
 
-    sendPingPacket() {
+    sendPing(){
         this.packet = Buffer.alloc(8);
 
         //write version
@@ -26,54 +26,39 @@ class Ping {
         this.packet.writeUInt8(0x0a, 6);
         this.packet.writeUInt8(0x09, 7);
 
-        //payload
-        var payload = new Buffer([
-                0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68
-            ]
-        );
+        //writes checksum
+        let sum = raw.createChecksum(this.packet);
+        this.packet.writeUInt16BE(sum, 2);
 
-        var bufArray = [this.packet, payload];
+        this.socket.send(this.packet, 0, this.packet.length, this.destinationIp, function () {}, function () {});
+    }
 
-        let segment = Buffer.concat(bufArray);
+    sendLargePingPacket(){
+        this.packet = Buffer.alloc(1000);
 
-        let sum = raw.createChecksum(segment);
-        segment.writeUInt16BE(sum, 2);
+        //write version
+        this.packet.writeUInt8(8, 0);
 
-        console.log(segment);
+        //write code
+        this.packet.writeUInt8(0, 1);
 
-        this.socket.send(segment, 0, segment.length, this.destinationIp, function () {
-        }, function () {
-        });
+        //write identifier
+        this.packet.writeUInt8(0x00, 4);
+        this.packet.writeUInt8(0x01, 5);
 
+        //write sequence number
+        this.packet.writeUInt8(0x0a, 6);
+        this.packet.writeUInt8(0x09, 7);
+
+        this.packet.fill(1, 8, this.packet.length);
+        console.log(this.packet);
+
+        //writes checksum
+        let sum = raw.createChecksum(this.packet);
+        this.packet.writeUInt16BE(sum, 2);
+
+        this.socket.send(this.packet, 0, this.packet.length, this.destinationIp, function () {}, function () {});
     }
 }
-
-
-// // ICMP echo (ping) request, checksum should be ok
-// var buffer = new Buffer ([
-//     type        code        checksum
-//     0x08, 0x00, 0x43, 0x52, 0x00, 0x01, 0x0a, 0x09,
-//     0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
-//     0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70,
-//     0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x61,
-//     0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69]);
-//
-// var socketLevel = raw.SocketLevel.IPPROTO_IP
-// var socketOption = raw.SocketOption.IP_TTL;
-//
-// function beforeSend () {
-//     socket.setOption (socketLevel, socketOption, 1);
-// }
-//
-// function afterSend (error, bytes) {
-//     if (error)
-//         console.log (error.toString ());
-//     else
-//         console.log ("sent " + bytes + " bytes");
-//
-//     socket.setOption (socketLevel, socketOption, 1);
-// }
-//
-// socket.send (buffer, 0, buffer.length, target, beforeSend, afterSend);
 
 module.exports = Ping;

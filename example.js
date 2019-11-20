c282b8e5c6b2cc47add93558847a787def68f79b
bhavayanand <bhavay.anand@intelliticks.com>
2019-11-21 01:06:47 +0530

    syn completed

--- example.js
+++ example.js
@@ -1,8 +1,9 @@
 let Ping = require('./ping');
+let Syn = require('./syn');
 
-let c = new Ping('15.206.102.11');
-
+// let c = new Ping('15.206.102.11');
 // c.sendPing();
-
 // c.sendLargePingPacket();
 
+// let s = new Syn(99, 80, '15.206.102.11');
+// s.sendSyn();
